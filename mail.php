fa947b70636cb8cd15395adcf5f658b61d72e232
delskieeee <88223619+delskieeee@users.noreply.github.com>
2021-10-30 12:12:38 +0800

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,4 +1,4 @@
 <?php
 
-mail('rafael.ricablanca001@gmail.com', 'Test Subject', 'Hello There!','From: rafael.ricablanca001@gmail.com')
+mail('nothingavailableid@gmail.com', 'Test Subject', 'Hello There!','From: nothingavailableid@gmail.com')
 ?>
